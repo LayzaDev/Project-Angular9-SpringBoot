ba96a6b9a9215fa78bcacd68d70f73d9fcefb31a
LayzaDev <layzanauanedev@gmail.com>
2024-12-30 23:57:53 -0300

    feat(Clients): add internationalization support for validation messages

--- src/main/java/layzadev/github/io/model/entity/Client.java
+++ src/main/java/layzadev/github/io/model/entity/Client.java
@@ -5,8 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -14,15 +17,19 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty(message = "{field.name.required}")
     private String name;
 
     @Column(nullable = false, length = 11)
+    @NotNull(message = "{field.cpf.required}")
+    @CPF(message = "{field.cpf.invalid}")
     private String cpf;
 
     @Column(name = "registration_date", updatable = false)
